0df5874a10e3a4a51584aadff1bf592180a4fde1
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-07-16 19:23:29 -0400

    install react router dom and add routes and some pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Room3DMap from "./Components/Room3DMap.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
 
     return (
         <>
         <Room3DMap />
+            <Router>
+                <Routes>
+                    {/* <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/about" element={<About />} /> */}
+                </Routes>
+            </Router>
         </>
     )
 
---
+++ src/Pages/About.jsx
@@ -0,0 +1,9 @@
+import "./About.css"
+
+export default function About() {
+    return (
+        <div className="about">
+            <p>Dino-Museum by: Runquan (Ray) Zhou</p>
+        </div>
+    )
+}
